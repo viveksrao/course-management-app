ab27617d04dbdc4a6f76cd34e665f66c5d4a92c9
Vivekanand Rao <vrao@egnyte.com>
2020-05-07 16:16:37 -0700

    Demo of Template Driven and Reactive Forms.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './template-driven-form/sign-up/sign-up.component';
+import { RegisterComponent } from './reactive-form/register/register.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: '',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CourseModule } from './courses/course.module';
+import { SignUpComponent } from './template-driven-form/sign-up/sign-up.component';
+import { RegisterComponent } from './reactive-form/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, CourseModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SignUpComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CourseModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
